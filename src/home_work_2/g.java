79970aba98be7f0c2d641031e5724c02af37a1df
YulSai <yluya.guo@gmail.com>
2022-03-27 16:47:13 +0300

    renamed folder'

---
+++ src/home_work_2/g.java
@@ -0,0 +1,4 @@
+package home_work_2;
+
+public class g {
+}
---
+++ src/home_work_2/loops/loops_Task01.java
@@ -0,0 +1,7 @@
+package home_work_2.loops;
+
+public class loops_Task01 {
+    public static void main(String[] args) {
+        
+    }
+}
